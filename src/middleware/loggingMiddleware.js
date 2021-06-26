5dc5b92b952ddb68cf414f809c9082895fb029ea
Mauro Parafati <mparafati@fi.uba.ar>
2021-06-26 17:14:35 -0300

    add query

--- src/middleware/loggingMiddleware.js
+++ src/middleware/loggingMiddleware.js
@@ -4,6 +4,10 @@ module.exports = function loggingMiddlewareFactory(logger) {
 			logger.debug('Request Headers: %s', req.headers);
 		}
 
+		if (req.query) {
+			logger.debug('Request Query: %s', req.query);
+		}
+
 		if (req.body) {
 			logger.debug('Request Body: %s', req.body);
 		}
