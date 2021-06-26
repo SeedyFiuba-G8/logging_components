403040b46566f438ca9401838a85dce27c1b4ea1
Mauro Parafati <mparafati@fi.uba.ar>
2021-06-26 17:38:00 -0300

    using lodash

--- src/middleware/loggingMiddleware.js
+++ src/middleware/loggingMiddleware.js
@@ -1,14 +1,16 @@
+const _ = require('lodash');
+
 module.exports = function loggingMiddlewareFactory(logger) {
 	return function loggingMiddleware(req, res, next) {
-		if (req.headers) {
+		if (!_.isEmpty(req.headers)) {
 			logger.debug('Request Headers: %s', req.headers);
 		}
 
-		if (req.query) {
+		if (!_.isEmpty(req.query)) {
 			logger.debug('Request Query: %s', req.query);
 		}
 
-		if (req.body) {
+		if (!_.isEmpty(req.body)) {
 			logger.debug('Request Body: %s', req.body);
 		}
 
